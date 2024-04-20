63c5c948e7109ce9d679a18b60511a847ee369b1
D-I-V-Y-A-S <divyaselvam1405@gmail.com>
2024-04-20 19:33:51 +0530

    forms

--- src/Components/BetterFormHandling/BetterFormHandling.jsx
+++ src/Components/BetterFormHandling/BetterFormHandling.jsx
@@ -1,54 +1,61 @@
 import React, { useState } from 'react'
+import './BetterFormHandling.css'
+const BetterFormHandling = () => {
 
-const BetterFormHandling= () => {
-    const [inputs, setInputs] = useState({})
+  const [inputs, setInputs] = useState({})
+  
+  //{}empty object.
 
-    const handleInput = (event) => {
-        const name = event.target.name
-        const value = event.target.value
-        setInputs(values =>({
-            ...values, [name]:value
-        }))
-    }
-    function handleRegistrationForm(event) {
-        event.preventDefault()
-        console.log(inputs.firstName)
-        console.log(inputs.lastName)
-        console.log(inputs.stateName)
-    }
+  const handleInput = (event) => {
+    const name = event.target.name
+    const value = event.target.value
+    setInputs(values => ({
+      ...values, [name]: value
+    }))
+  }
+  //...values->spread operator -no need of mentioning size expands to incoming values.
+  function handleRegistrationForm(event) {
+    event.preventDefault()
+    console.log(inputs)
+    console.log(inputs.firstName)
+    console.log(inputs.lastName)
+    console.log(inputs.stateName)
+  }
   return (
     <React.Fragment>
-      <form>
-        <label htmlFor='firstName'>First Name</label>
-        <input
-          type='text'
-          placeholder='Enter your first name'
-          id='firstName'
-          name='firstName'
-          value={inputs.firstName}
-          onChange={handleInput}
-        />
-        <label htmlFor='lastName'>Last Name</label>
-        <input
-          type='text'
-          placeholder='Enter your last name'
-          id='lastName'
-          name='lastName'
-          value={inputs.lastName}
-          onChange={handleInput}
-        />
-        
-        <label htmlFor='stateName'>State</label>
-        <select id='stateName' name='stateName' value={inputs.stateName} onChange={handleInput}>
-        <option value=''>select</option>
-          <option value='Tamilnadu'> Tamilnadu</option>
-          <option value='Kerala'> Kerala</option>
-          <option value='Andra Pradesh'> Andhra Pradesh</option>
-        </select>
-
-        <button onClick={handleRegistrationForm}>Click me!!</button>
-
-      </form>
+      <div className="field">
+        <p className="topic">Better Form Handling</p>
+        <form onSubmit={handleRegistrationForm}>
+          <label htmlFor='firstName' className="label-name">First Name</label>
+          <input className="input-field"
+            type='text'
+            placeholder='Enter your first name'
+            id='firstName'
+            name='firstName'
+            value={inputs.firstName} 
+            onChange={handleInput}
+          />
+          <label htmlFor='lastName' className="label-name">Last Name</label>
+          <input className="input-field"
+            type='text'
+            placeholder='Enter your last name'
+            id='lastName'
+            name='lastName'
+            value={inputs.lastName}
+            onChange={handleInput}
+          />
+          <label htmlFor='stateName' className="label-name">State</label>
+          <select id='stateName' name='stateName' value={inputs.stateName} onChange={handleInput}>
+            <option value='' className="label-select" >select</option>
+            <option value='Tamilnadu'> Tamilnadu</option>
+            <option value='Kerala'> Kerala</option>
+            <option value='Andra Pradesh'> Andhra Pradesh</option>
+          </select>
+          <div className='btn'>
+            <button type="submit">Click me!!</button>
+          </div>
+        </form>
+      </div>
     </React.Fragment>
   )
 }
--- src/Components/RegistrationFormComponent/RegistrationFormComponent.jsx
+++ src/Components/RegistrationFormComponent/RegistrationFormComponent.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-
+import './RegistrationFormHandling.css';
 const RegistrationFormComponent = () => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [stateName, setStateName] = useState('');
+    const [Registernumber, setRegisternumber] = useState('');
 
     const handleFirstName = (event) => {
         setFirstName(event.target.value);
@@ -16,50 +17,81 @@ const RegistrationFormComponent = () => {
     const handleStateName = (event) => {
         setStateName(event.target.value);
     }
+    const handleRegisternumber = (event) => {
+        setRegisternumber(event.target.value);
+    }
 
     const handleRegistrationForm = (event) => {
+
         event.preventDefault();
+        //text place in field doesn't disappear.
         console.log(firstName);
         console.log(lastName);
         console.log(stateName);
+
     }
 
     return (
         <React.Fragment>
-            <form onSubmit={handleRegistrationForm}>
-                <label htmlFor='firstName'></label>
-                <input
-                    type='text'
-                    placeholder='Enter your first name'
-                    id='firstName'
-                    name='firstname'
-                    value={firstName}
-                    onChange={handleFirstName}
-                />
+            <div className="field">
+                <p className="topic">Form Handling</p>
+                <form onSubmit={handleRegistrationForm}>
+                    <label htmlFor='firstName' className="label-name">First Name</label>
+                    <input className="input-field"
+                        type='text'
+                        placeholder='Enter your first name'
+                        id='firstName'
+                        name='firstname'
+                        value={firstName}
+                        onChange={handleFirstName}
+                    />
+                    <br />
 
-                <label htmlFor='lastName'></label>
-                <input
-                    type='text'
-                    placeholder='Enter your last name'
-                    id='lastName'
-                    name='lastname'
-                    value={lastName}
-                    onChange={handleLastName}
-                />
+                    <label htmlFor='lastName' className="label-name">Last Name</label>
+                    <input className="input-field"
+                        type='text'
+                        placeholder='Enter your last name'
+                        id='lastName'
+                        name='lastname'
+                        value={lastName}
+                        onChange={handleLastName}
+                    />
+                    <br />
+                    <label htmlFor='Rno' className="label-name">Registration Number</label>
+                    <input
+                        type="number"
+                        name="Rno"
+                        id="registerNumber"
+                        className="input-field"
+                        placeholder='Register Number'
+                        value={Registernumber}
+                        onChange={handleRegisternumber}
+                    />
+                    <br />
+                    {/* { console.log({Registernumber} )} */}
+                    <label htmlFor='stateName' className="label-name">Select your state</label>
+                    <select name="stateName" id="stateName" value={stateName} onChange={handleStateName}>
+                        <option value='' className="label-select">select</option>
+                        <option value="TamilNadu">TamilNadu</option>
+                        <option value="Kerala">Kerala</option>
+                        <option value="Karnataka">Karnataka</option>
 
-                <label htmlFor='stateName'></label>
-                <select name="stateName" id="stateName" value={stateName} onChange={handleStateName}>
-                <option value=''>select</option>
-                    <option value="TamilNadu">TamilNadu</option>
-                    <option value="Kerala">Kerala</option>
-                    <option value="Karnataka">Karnataka</option>
-                </select>
+                    </select>
 
-                <button type="submit">Click me!</button>
-            </form>
+                    <br />
+                    <div className='btn'>
+                        <button type="submit">Click me!</button>
+                    </div>
+                </form>
+            </div>
         </React.Fragment>
-    );
+    )
 }
 
 export default RegistrationFormComponent;
 
+/*
+1.Create usestate for every label.
+2.Assign values through setstate.
+3.Log the values on submitting the form.
+*/
